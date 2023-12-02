a71d68cbc9cae88c9a6b802cfa25b1b97df93b97
kajal-2011 <kajalrekha888@gmail.com>
2023-12-02 11:37:52 +0600

    navbar added

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header className="fixed left-0 right-0 top-0 z-[100] flex h-20 w-full items-center border-b border-gray bg-white/90 backdrop-blur-xl">
+      {/* NAV LEFT */}
+      <nav className="wrapper flex items-center justify-between gap-5">
+        <Link href="/" className="text-2xl font-semibold">
+          luminex
+        </Link>
+
+        {/* NAV RIGHT */}
+
+        <ul className="flex items-center gap-5 text-lg">
+          <li>
+            <Link href="/#home" className="link-item">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/#products" className="link-item">
+              Products
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/about" className="link-item">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog" className="link-item">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="link-item">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link href="/sign-in">Sign in</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;

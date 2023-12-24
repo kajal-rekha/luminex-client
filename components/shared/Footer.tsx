77413b8b7f94d0b01d542896399a1f9666ce871a
kajal-2011 <kajalrekha888@gmail.com>
2023-12-24 19:43:32 +0600

    footer done

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -7,20 +7,25 @@ const Footer = () => {
     <footer className='footer'>
       <div className='footer-bg'>
         <Overlay />
-        <div className='wrapper sp grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5 md:gap-6 lg:gap-4 '>
+        <div className='wrapper sp grid grid-cols-2  lg:grid-cols-3  xl:grid-cols-5 gap-10 md:gap-8 lg:gap-6 xl:mt-12 2xl:mt-24'>
           <div className='w-full'>
-            <Link href='/' className='text-3xl font-semibold text-orange/70'>
+            <Link
+              href='/'
+              className='text-xl lg:text-3xl font-semibold text-orange/70'
+            >
               Luminex
             </Link>
-            <p className='text-gray/80 mt-5 w-full leading-8'>
-              Discover Luminex, my unique jewelry store online. Luminex is your
-              destination for distinctive, fashionable jewelry.
+            <p className='text-gray/80 mt-5 w-full leading-8 text-sm lg:text-lg'>
+              Discover Luminex, my <br /> unique jewelry store online. <br />{" "}
+              Luminex is your destination for distinctive, fashionable jewelry.
             </p>
           </div>
-          <div className='w-full xl:ml-20 space-y-4'>
-            <h2 className='text-2xl font-semibold text-gray/80 '>Services</h2>
-            <ul className='flex flex-col gap-5 text-lg'>
-              <li>
+          <div className='w-full '>
+            <h2 className='text-xl md:text-2xl font-semibold text-gray/80 '>
+              Quick links
+            </h2>
+            <ul className='flex flex-col  space-y-4'>
+              <li className='mt-3'>
                 <Link
                   href='/'
                   className='link-item text-gray/80 hover:text-orange/80 eq'
@@ -46,7 +51,14 @@ const Footer = () => {
                   About
                 </Link>
               </li>
-              <li>
+            </ul>
+          </div>
+          <div className='w-full'>
+            <h2 className='text-xl md:text-2xl font-semibold text-gray/80 '>
+              More links
+            </h2>
+            <ul className='space-y-4'>
+              <li className='mt-3'>
                 <Link
                   href='/testimonial'
                   className='link-item text-gray/80 hover:text-orange/80 eq'
@@ -72,36 +84,32 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className='w-full xl:ml-20 space-y-4'>
-            <h2 className='text-2xl font-semibold text-gray/80 '>Services</h2>
-            <ul className='text-gray/80 gap-3 flex flex-col'>
-              <li className='hover:text-orange/80 eq'>Shipping Policy</li>
-              <li className='hover:text-orange/80 eq'>Compensation First</li>
-              <li className='hover:text-orange/80 eq'>Return Policy</li>
-
-              <li className='hover:text-orange/80 eq'>Shipping Info</li>
-            </ul>
-          </div>
-          <div className='w-full space-y-4'>
-            <h2 className='text-2xl text-gray/80 font-semibold  '>
-              Information
+          <div className='w-full '>
+            <h2 className='text-xl md:text-2xl font-semibold text-gray/80 '>
+              Services
             </h2>
-            <ul className='space-y-4'>
-              <li className='text-gray/80 hover:text-orange/80 eq'>About Us</li>
-              <li className='text-gray/80 hover:text-orange/80 eq'>
-                Delivery Information
+            <ul className=' flex flex-col space-y-4'>
+              <li className='text-gray/80 hover:text-orange/80 eq mt-3'>
+                Shipping Policy
               </li>
               <li className='text-gray/80 hover:text-orange/80 eq'>
-                Privacy Policy
+                Compensation First
+              </li>
+              <li className='text-gray/80 hover:text-orange/80 eq'>
+                Return Policy
+              </li>
+
+              <li className='text-gray/80 hover:text-orange/80 eq'>
+                Shipping Info
               </li>
             </ul>
           </div>
-          <div className='w-full space-y-4'>
-            <h2 className='text-2xl text-gray/80 font-semibold  '>
+          <div className='w-full '>
+            <h2 className='text-xl md:text-2xl text-gray/80 font-semibold  '>
               Contact Us
             </h2>
             <ul className='space-y-4'>
-              <li className='text-gray/80 hover:text-orange/80 eq'>
+              <li className='text-gray/80 hover:text-orange/80 eq mt-3'>
                 Email: luminex123@gmail.com
               </li>
               <li className='text-gray/80 hover:text-orange/80 eq'>
@@ -111,31 +119,28 @@ const Footer = () => {
                 Address: No 40 Baria Sreet 133/2, NewYork, USA
               </li>
             </ul>
+            <div className=' social-icons mt-5 flex gap-10'>
+              <Link
+                href='https://www.facebook.com/anowar.kajal'
+                className='border border-gray/80 p-2 rounded-full hover:bg-orange/80  eq '
+              >
+                <FaFacebookF className='text-xl text-gray/80 ' />
+              </Link>
+              <Link
+                href='https://www.linkedin.com/in/kajal-rekha-81262b264'
+                className='border border-gray/80 p-2 rounded-full hover:border-orange/80  duration-500'
+              >
+                <FaLinkedinIn className='text-xl text-gray/80' />
+              </Link>
+              <Link
+                href='https://twitter.com/KajalRekha14'
+                className='border border-gray/80 p-2 rounded-full hover:border-orange/80  duration-500'
+              >
+                <FaTwitter className='text-xl text-gray/80' />
+              </Link>
+            </div>
           </div>
           O
-        </div>
-
-        <div className=''>
-          <div className=' social-icons justify-center items-center mx-auto ml-[8%] -mt-20 flex gap-10'>
-            <Link
-              href='https://www.facebook.com/anowar.kajal'
-              className='border border-gray/80 p-2 rounded-full hover:border-orange/80  duration-500 '
-            >
-              <FaFacebookF className='text-xl text-gray/80 ' />
-            </Link>
-            <Link
-              href='https://www.linkedin.com/in/kajal-rekha-81262b264'
-              className='border border-gray/80 p-2 rounded-full hover:border-orange/80  duration-500'
-            >
-              <FaLinkedinIn className='text-xl text-gray/80' />
-            </Link>
-            <Link
-              href='https://twitter.com/KajalRekha14'
-              className='border border-gray/80 p-2 rounded-full hover:border-orange/80  duration-500'
-            >
-              <FaTwitter className='text-xl text-gray/80' />
-            </Link>
-          </div>
         </div>
       </div>
       <div className='bg-gray/90 p-10'>
